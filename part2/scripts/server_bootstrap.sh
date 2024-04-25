0fe8d21643b5da34bbe9723a23b157bb12fcace8
Aurh1l <Aurh1l@example.com>
2024-04-25 13:40:22 +0200

    fix confs path

--- part2/scripts/server_bootstrap.sh
+++ part2/scripts/server_bootstrap.sh
@@ -2,7 +2,7 @@
 apt-get update
 export INSTALL_K3S_EXEC="--node-ip 192.168.56.110"
 curl -sfL https://get.k3s.io | sh -
-kubectl apply -f /vagrant/base/ingress.yaml
-kubectl apply -k /vagrant/overlays/a
-kubectl apply -k /vagrant/overlays/b
-kubectl apply -k /vagrant/overlays/c
+kubectl apply -f /vagrant/confs/base/ingress.yaml
+kubectl apply -k /vagrant/confs/overlays/a
+kubectl apply -k /vagrant/confs/overlays/b
+kubectl apply -k /vagrant/confs/overlays/c
