c634956d130e537260556068d2cd6bbffa280ad5
Aurh1l <Aurh1l@example.com>
2024-04-25 15:56:55 +0200

    fix part3 by installing tools on first VM and delete useless vagrantfile \ rename DNS wil.iot and argocd.iot

---
+++ part3/scripts/startup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Setup K3D
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# Setup Kubectl
+curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Setup Cluster
+k3d cluster create dev-cluster --port 8080:80@loadbalancer --port 443:443@loadbalancer
+
+# Setup ArgoCD
+
+  # Setup ArgoCD cli
+  curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+  sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+  rm argocd-linux-amd64
+
+  # Apply Configurations
+  kubectl create namespace argocd
+  kubectl apply -k /vagrant/part3/confs/argocd
+
+# Wait for ArgoCD to be deployed
+kubectl wait --for=condition=Ready pods --all -n argocd --timeout=15m
+
+# Create Ingress Configuration for application
+kubectl create namespace dev
+kubectl apply -f /vagrant/part3/confs/ingress.yaml
+kubectl apply -f /vagrant/part3/confs/ingressDev.yaml
+
+# Log-in to ArgoCD
+until argocd admin initial-password -n argocd 2>/dev/null;do printf "\rWaiting for argocd to be ready...";done
+export "ARGO_PASS"="$(argocd admin initial-password -n argocd | head -n1)"
+argocd --grpc-web login argocd.iot --username admin --password "$ARGO_PASS" --insecure
+
+# Deploy Application
+argocd  --grpc-web app create dev --repo https://github.com/bramarien/IOT-app.git --path devWil --dest-server https://kubernetes.default.svc --dest-namespace dev --insecure
+argocd  --grpc-web app set dev --sync-policy automated --auto-prune --self-heal
