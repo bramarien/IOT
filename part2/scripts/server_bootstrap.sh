fc3a5c2352608f60f3dd6964b251f2b83f5a3dc6
Aurh1l <Aurh1l@example.com>
2024-04-25 13:47:19 +0200

    rename apps

--- part2/scripts/server_bootstrap.sh
+++ part2/scripts/server_bootstrap.sh
@@ -3,6 +3,6 @@ apt-get update
 export INSTALL_K3S_EXEC="--node-ip 192.168.56.110"
 curl -sfL https://get.k3s.io | sh -
 kubectl apply -f /vagrant/confs/base/ingress.yaml
-kubectl apply -k /vagrant/confs/overlays/a
-kubectl apply -k /vagrant/confs/overlays/b
-kubectl apply -k /vagrant/confs/overlays/c
+kubectl apply -k /vagrant/confs/overlays/app1
+kubectl apply -k /vagrant/confs/overlays/app2
+kubectl apply -k /vagrant/confs/overlays/app3
