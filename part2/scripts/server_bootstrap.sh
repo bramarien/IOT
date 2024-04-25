1d98da78df79ea2b41ea928ce342b8906605f1f6
Aurh1l <Aurh1l@example.com>
2024-04-25 14:31:24 +0200

    clean \ add scripts to install k3s \ wait for applications

--- part2/scripts/server_bootstrap.sh
+++ part2/scripts/server_bootstrap.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
+# Update packages
 apt-get update
-export INSTALL_K3S_EXEC="--node-ip 192.168.56.110"
-curl -sfL https://get.k3s.io | sh -
+
+# Installation of k3s
+
+  # Define environment variables
+    # Version of k3s to download from github
+      export INSTALL_K3S_VERSION="v1.28.5+k3s1"
+
+  # Execute install script
+  bash /vagrant/scripts/k3s_install.sh --bind-address 192.168.56.110 --node-ip 192.168.56.110
+
+# Configure applications
 kubectl apply -f /vagrant/confs/base/ingress.yaml
 kubectl apply -k /vagrant/confs/overlays/app1
 kubectl apply -k /vagrant/confs/overlays/app2
 kubectl apply -k /vagrant/confs/overlays/app3
+
+# Wait for applications to be ready
+kubectl wait --for=condition=Ready pods --all --timeout=3m
+
+echo "Server is now ready !"
+echo
+echo "Access applications on url: "
+echo "http://app1.com:8080/"
+echo "http://app2.com:8080/"
+echo "http://app3.com:8080/"
